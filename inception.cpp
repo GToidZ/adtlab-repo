b88fdf9a7aee5f535e3b7d39e5c1e9413cdf1ea7
GToidZ <amazijapan@gmail.com>
2022-10-03 19:29:07 +0700

    Added a solution for inception uva13055

--- inception.cpp
+++ inception.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-const int MAX_N = 10010;
-char names[MAX_N][20];
-
-// TODO: Make a stack implementation.
-
 struct Node {
-  char *name;
+  char name[20];
   Node *next;
 
-  Node(char *name, Node *next=0)
-    : name{name}, next(next) {}
+  Node(Node *next=nullptr):
+    next(next) {}
 };
+
+Node *top = nullptr;
 
 int main() {
   char cmd[10];
   int n;
   cin >> n;
-  for (int i = 0; n < i; i++) {
-    
+  for (int i = 0; i < n; i++) {
+    cin >> cmd;
+    if (cmd[0] == 'S') {
+      // <name> goes to sleep, add to stack.
+      char name[20];
+      cin >> name;
+      Node *person = new Node();
+      strcpy(person->name, name);
+      if (top != nullptr) {
+        person->next = top;
+      } 
+      top = person;
+    } else if (cmd[0] == 'K') {
+      // Kick the top most person out the stack.
+      if (top != nullptr) {
+        Node *next = top->next;
+        delete top;
+        top = next;
+      }
+    } else if (cmd[0] == 'T') {
+      // See what person is on the top of stack.
+      if (top != nullptr) {
+        cout << top->name << endl;
+      } else {
+        cout << "Not in a dream" << endl;
+      }
+    }
   }
-  //Node *john = new Node((char*)"John");
 }
